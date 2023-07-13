71d89bb30ceb02329a04af9834db856297cf5db2
Arka Biswas <arka.bttf88@gmail.com>
2023-07-13 11:56:53 +0530

    Attempt at improving cell generation time

--- sudoku.py
+++ sudoku.py
@@ -4,26 +4,34 @@ import random
 
 def initCellGen(table):
     for cell in range(0,9,3): #Cell here should represent the cell number we are working with
-        fillCell(table,cell)
+        initFillCell(table,cell,cell)
     return table
 
-def fillCell(table,cell): #Plan here is we would essentially be accessing the diagonal elements. 
+#Would generate the diagonal cells
+def initFillCell(table,row,col): 
     nolist = [1,2,3,4,5,6,7,8,9]
+    nolist_cp = nolist
     for i in range(3):
         for j in range(3):
             while True:
-                num = random.choice(nolist)
-                if unUsedNum(table,cell,num):
-                    break        
-            table[cell+i][cell+j] = num
+                if len(nolist_cp) < 2:
+                    num = nolist_cp[0]
+                else: 
+                    num = random.choice(nolist_cp)
+                if unUsedNum(table,row,col,num):
+                    break
+                else:
+                    nolist_cp.remove(num)
+            table[row+i][col+j] = num
             nolist.remove(num)
+            nolist_cp = nolist
 
 
 #Should check for all numbers in particular cell
-def unUsedNum(table,cell,num):
+def unUsedNum(table,row,col,num):
     for i in range(3):
         for j in range(3):
-            if table[cell+i][cell+j] == num:
+            if table[row+i][col+j] == num:
                 return False
     return True
 
