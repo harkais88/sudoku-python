c1b13c3cbe30bcde537c1c55cbd23cffd5a74e89
harkais88 <63041960+harkais88@users.noreply.github.com>
2024-02-07 02:01:59 +0530

    Added a prototype menu system

--- SudokuGame/player.py
+++ SudokuGame/player.py
@@ -20,6 +20,8 @@ class Game:
         self.screen.fill(self.background_fill) 
         self.running = True 
         self.clock = pygame.time.Clock()
+        self.start = False
+        self.start_flag = 0
         self.game = RSudoku.sudoku(diff) # Intializing game variable, has the grid and puzzle as attributes
         self.p = 493 / 9 #Obtained by Trial and Error
         self.i = self.j = 10 #Used for validating mouse pos and entering values
@@ -53,6 +55,17 @@ class Game:
                 # Getting Mouse Position Data
                 if event.type == pygame.MOUSEBUTTONUP:
                     pos = pygame.mouse.get_pos()
+                    # Following occurs only in main_menu
+                    if self.start == False:
+                        # If Quit Button is pressed
+                        if self.width // 2 - self.p < pos[0] < self.width // 2 + self.p and \
+                            self.width // 2 + self.p < pos[1] < self.width // 2 + 3*self.p:
+                            self.quit()
+                        # If START Button is pressed
+                        if self.width // 2 - self.p < pos[0] < self.width // 2 + self.p and \
+                                self.width //2 - self.p < pos[1] < self.width // 2 + self.p:
+                            self.start = True
+                            self.start_flag = 1
                     # Checks whether mouse click was within grid
                     if (self.p < pos[0] < self.width - self.p) and (self.p < pos[1] < self.width - self.p):
                         self.i,self.j = int(pos[1]//self.p), int(pos[0]//self.p)                      
@@ -89,72 +102,98 @@ class Game:
 
     def draw(self):
         """Responsible for drawing at every frame"""
-        # Drawing the lines
-        for i in range(10):
-            if i % 3 == 0: line_width = 4;
-            else: line_width = 2;
-            # Vertical Line
-            pygame.draw.line(self.screen, (0,0,0), 
-                             (self.p + (self.p*i), self.p), (self.p + (self.p * i),self.width - self.p), 
-                             width = line_width)
-            # Horizontal Line
-            pygame.draw.line(self.screen, (0,0,0), 
-                             (self.p, self.p + (self.p * i)), (self.width - self.p,self.p + (self.p * i)), 
-                             width = line_width)
+        if self.start == False:
+            # Main Menu
+            main_font = pygame.font.SysFont("Comic Sans MS",30)
 
-        # Updating Screen
-        pygame.display.update()
+            # Start Button Operations
+            start_text = main_font.render("START",True,(255,0,0))
+            pygame.draw.rect(self.screen, (0,0,0), 
+                             (self.width // 2 - self.p, self.width // 2 - self.p, 2*self.p, self.p))
+            self.screen.blit(start_text, (self.width // 2 - self.p + 2, self.width // 2 - self.p + 2))
 
-        # Setting Font Style And Colors
-        num_font = pygame.font.SysFont("Comic Sans MS",30) # Setting Font of the Numbers
-        sel_color = (177,232,237)
-        num_color = (56,123,232)
-        sol_color = (19,214,120)
-        err_color = (247,27,20)
+            # Quit Button Operations
+            quit_text = main_font.render("QUIT",True,(255,0,0))
+            pygame.draw.rect(self.screen, (0,0,0), 
+                             (self.width // 2 - self.p, self.width // 2 + self.p, 2*self.p, self.p))
+            self.screen.blit(quit_text, (self.width // 2 - self.p + 10, self.width // 2 + self.p + 2))
+            pygame.display.update()
+        else:
+            if self.start_flag == 1:
+                # If Start Button is pressed, we remove the Main Screen Buttons
+                # The flag is given so that this does not happen for every frame
+                rmv_start = pygame.draw.rect(self.screen, self.background_fill, 
+                            (self.width // 2 - self.p, self.width // 2 - self.p, 2*self.p, self.p))
+                rmv_quit = pygame.draw.rect(self.screen, self.background_fill,
+                            (self.width // 2 - self.p, self.width // 2 + self.p, 2*self.p, self.p))
+                pygame.display.update([rmv_start,rmv_quit])
+                self.start_flag = 0
+            # Drawing the lines
+            for i in range(10):
+                if i % 3 == 0: line_width = 4;
+                else: line_width = 2;
+                # Vertical Line
+                pygame.draw.line(self.screen, (0,0,0), 
+                                (self.p + (self.p*i), self.p), (self.p + (self.p * i),self.width - self.p), 
+                                width = line_width)
+                # Horizontal Line
+                pygame.draw.line(self.screen, (0,0,0), 
+                                (self.p, self.p + (self.p * i)), (self.width - self.p,self.p + (self.p * i)), 
+                                width = line_width)
 
-        # Instruction
-        inst = num_font.render("Select, Then Enter Number", True, (9,23,46))
-        self.screen.blit(inst,(0,0))
+            # Updating Screen
+            pygame.display.update()
 
-        # Coloring Selected Cell
-        if self.flag == 1:
-            # The worst way to clear a already selected cell :(
-            # Could probably fix this if the previous selected cell coords are recorded, will do this later
-            for i in range(int(self.p), int(self.width - self.p)):
-                for j in range(int(self.p), int(self.width - self.p)):
-                    if self.screen.get_at((i,j)) == sel_color:
-                        self.screen.set_at((i,j),self.background_fill)
-            # Providing Selection Color to Selected Cell
-            selector = pygame.Surface((self.p,self.p))
-            selector.fill(sel_color)
-            self.screen.blit(selector, (self.j*self.p, self.i*self.p))
-            self.flag = 0
+            # Setting Font Style And Colors
+            num_font = pygame.font.SysFont("Comic Sans MS",30) # Setting Font of the Numbers
+            sel_color = (177,232,237)
+            num_color = (56,123,232)
+            sol_color = (19,214,120)
+            err_color = (247,27,20)
 
-        # Rendering the puzzle
-        for i in range(9):
-            for j in range(9):
-                if self.game.puzzle[i][j] != 0:
-                    puzzle_num = num_font.render(str(self.game.puzzle[i][j]), True, num_color)
-                    self.screen.blit(puzzle_num, ((j+1)*self.p + 20, (i+1)*self.p + 5)) #Values found from T&E
+            # Instruction
+            inst = num_font.render("Select, Then Enter Number", True, (9,23,46))
+            self.screen.blit(inst,(0,0))
 
-        if self.value != "0":
-            # Inputting the value into our solution for checking with our grid 
-            self.solution[self.i-1][self.j-1] = int(self.value)
-            
-            # Any value in the selected cell will be removed
-            whitener = pygame.Surface((self.p, self.p))
-            whitener.fill(self.background_fill)
-            whitener_rect = self.screen.blit(whitener, (self.j * self.p, self.i * self.p))
-            pygame.display.update(whitener_rect)
+            # Coloring Selected Cell
+            if self.flag == 1:
+                # The worst way to clear a already selected cell :(
+                # Could probably fix this if the previous selected cell coords are recorded, will do this later
+                for i in range(int(self.p), int(self.width - self.p)):
+                    for j in range(int(self.p), int(self.width - self.p)):
+                        if self.screen.get_at((i,j)) == sel_color:
+                            self.screen.set_at((i,j),self.background_fill)
+                # Providing Selection Color to Selected Cell
+                selector = pygame.Surface((self.p,self.p))
+                selector.fill(sel_color)
+                self.screen.blit(selector, (self.j*self.p, self.i*self.p))
+                self.flag = 0
 
-            # Checking our input ... I should probably change this to where I cant do this,
-            # then give a check button, which would then show which ones are wrong
-            if self.game.grid[self.i-1][self.j-1] != int(self.value):
-                value = num_font.render(self.value, True, err_color)
-            else:
-                value = num_font.render(self.value, True, sol_color)
-            self.screen.blit(value, (self.j * self.p + 20, self.i * self.p + 5))
-            self.value = "0"
+            # Rendering the puzzle
+            for i in range(9):
+                for j in range(9):
+                    if self.game.puzzle[i][j] != 0:
+                        puzzle_num = num_font.render(str(self.game.puzzle[i][j]), True, num_color)
+                        self.screen.blit(puzzle_num, ((j+1)*self.p + 20, (i+1)*self.p + 5)) #Values found from T&E
+
+            if self.value != "0":
+                # Inputting the value into our solution for checking with our grid 
+                self.solution[self.i-1][self.j-1] = int(self.value)
+                
+                # Any value in the selected cell will be removed
+                whitener = pygame.Surface((self.p, self.p))
+                whitener.fill(self.background_fill)
+                whitener_rect = self.screen.blit(whitener, (self.j * self.p, self.i * self.p))
+                pygame.display.update(whitener_rect)
+
+                # Checking our input ... I should probably change this to where I cant do this,
+                # then give a check button, which would then show which ones are wrong
+                if self.game.grid[self.i-1][self.j-1] != int(self.value):
+                    value = num_font.render(self.value, True, err_color)
+                else:
+                    value = num_font.render(self.value, True, sol_color)
+                self.screen.blit(value, (self.j * self.p + 20, self.i * self.p + 5))
+                self.value = "0"
     
     def quit(self):
         """Quits the Game"""
